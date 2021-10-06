22bc2732a2b9533c6d6382156a0ec10e318d6341
Onnimanni Hannonen <onnari11@gmail.com>
2021-10-06 15:07:57 +0300

    added systemNavigationBar properties to SystemUiOverlayStyles

--- lib/src/styles.dart
+++ lib/src/styles.dart
@@ -4,12 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 SystemUiOverlayStyle getLightStatusBar() => const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
       statusBarColor: Colors.transparent,
       statusBarBrightness: Brightness.dark,
       statusBarIconBrightness: Brightness.light,
     );
 
 SystemUiOverlayStyle getDarkStatusBar() => const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.black,
+      systemNavigationBarIconBrightness: Brightness.light,
       statusBarColor: Colors.transparent,
       statusBarBrightness: Brightness.light,
       statusBarIconBrightness: Brightness.dark,
